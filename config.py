4094ea23a9a05c68a86e9ce7dd8fef4f384d5bfe
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2023-03-04 20:52:31 +0300

    Revert "ReStart BOT"
    This reverts commit e25c4d514b34da4a3a0613a6f22e31932dc3523b.

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 
 TOKEN = '6259371368:AAFuMuz9gDdVz6OCJ_UPN0MThfY4kU-Qn28'
 
-CHANNEL_URL = "https://t.me/my_bot_python"
-CHANNEL_ID = "@my_bot_python"
-CHAT_ID = "@my_bot_python"
+GROUP_ID = -1001573258786
 
-ADMIN_ID = "639483037"
+
